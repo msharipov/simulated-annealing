78cce9248de07d09336e75cac70eb770f01d3827
msharipov <msharipovr@gmail.com>
2024-03-08 18:03:43 -0500

    refactor: fixed formatting

--- parameters.cpp
+++ parameters.cpp
@@ -19,20 +19,26 @@ const std::vector<coord_diff> ONEACROSS{
     coord{2, 0}
 };
 
-double potential(const Matrix<int_fast8_t> &mat, const size_t r, const size_t c,
-                 const bool wrap) {
+double
+potential(const Matrix<int_fast8_t> &mat, const size_t r, const size_t c,
+          const bool wrap) {
 
     double V = 0;
 
     auto close = valid_neighbors(r, c, HEIGHT, WIDTH, CLOSEST8);
+
     for (const auto & n : *close) {
+
         V += 4 * mat[n.first][n.second];
     }
 
     auto far = valid_neighbors(r, c, HEIGHT, WIDTH, ONEACROSS);
     int_fast8_t far_count = 1;
+
     for (const auto & n : *far) {
+
         if (mat[n.first][n.second] == 1) {
+
             far_count *= 2;
         }
     }
@@ -45,6 +51,7 @@ double potential(const Matrix<int_fast8_t> &mat, const size_t r, const size_t c,
 bool accept(const double V_diff, const double T) {
 
     if (V_diff < 0) {
+
         return true;
     }
 
--- parameters.hpp
+++ parameters.hpp
@@ -17,11 +17,6 @@ static constexpr bool WRAPPED = true;
 
 static constexpr uint32_t MAX_STEPS = 500000;
 
-typedef std::pair<size_t, size_t> coord;
-typedef std::pair<ptrdiff_t, ptrdiff_t> coord_diff;
-typedef std::pair<std::array<size_t, HEIGHT>, std::array<size_t, WIDTH>> path;
-
-<<<<<<< HEAD
 typedef std::pair<size_t, size_t>                   coord;
 typedef std::pair<ptrdiff_t, ptrdiff_t>             coord_diff;
 typedef std::pair<std::array<size_t, HEIGHT>,
@@ -31,10 +26,6 @@ template <typename T>
 using Matrix = std::array<std::array<T, WIDTH>, HEIGHT>;
 template <typename T>
 using up = std::unique_ptr<T>;
-=======
-template <typename T> using mat_t = std::array<std::array<T, WIDTH>, HEIGHT>;
-template <typename T> using up = std::unique_ptr<T>;
->>>>>>> 8f009d43fd8e653c3661fc16f3a809369d264341
 
 extern const std::vector<coord_diff> CLOSEST8;
 
@@ -48,15 +39,11 @@ valid_neighbors(const size_t r, const size_t c, const ptrdiff_t max_r,
                 const ptrdiff_t max_c, const std::vector<coord_diff> &offsets,
                 const bool wrap = true);
 
-<<<<<<< HEAD
-extern double potential(const Matrix<int_fast8_t> & mat,
-                        const size_t r, const size_t c,
-                        const bool wrap=true);
-=======
-extern double potential(const mat_t<int_fast8_t> &mat, const size_t r,
-                        const size_t c, const bool wrap = true);
->>>>>>> 8f009d43fd8e653c3661fc16f3a809369d264341
+extern double
+potential(const Matrix<int_fast8_t> & mat, const size_t r, const size_t c,
+          const bool wrap=true);
 
-extern bool accept(const double V_diff, const double T);
+extern bool
+accept(const double V_diff, const double T);
 
 #endif
